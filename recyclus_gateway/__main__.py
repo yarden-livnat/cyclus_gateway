8794f8cfb916eae8dac3d8e4b419d34f74d92891
Yarden Livnat <yarden.livnat@gmail.com>
2019-04-30 14:33:40 -0600

    minor

--- recyclus_gateway/__main__.py
+++ recyclus_gateway/__main__.py
@@ -1,6 +1,5 @@
 import os
 from .__init__ import create_app
 
-app = create_app(os.getenv('FLASK_ENV') or 'development')
-# app = create_app()
-app.run(host='0.0.0.0', port=5000, debug=True)
+app = create_app(os.getenv('FLASK_ENV') or 'production')
+app.run(host='0.0.0.0', port=5000, debug=False)
